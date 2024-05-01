948735c9e800bea8aa4a5007688b67ee0c7fc8c5
jacquelyn-bradshaw <jacquelyn_bradshaw@live.co.uk>
2024-05-01 14:42:29 +0100

    NoProjectSelected and Button Components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Sidebar from "./Components/Sidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSeleted.jsx";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
