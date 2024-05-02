b7f158c2516c5764862425f4364d3ace171b82a2
jacquelyn-bradshaw <jacquelyn_bradshaw@live.co.uk>
2024-05-02 10:47:51 +0100

    created state to enable add project buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import Sidebar from "./Components/Sidebar.jsx";
+import {useState} from "react";
+
+import Sidebar from "./components/Sidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSeleted.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleAddProjectButton () {
+    setProjectsState(prevProjectsState => {
+      return {
+      ...prevProjectsState,
+      selectedProjectId: null
+    }})
+  }
+
+  let pageContent;
+
+  if (projectsState.selectedProjectId === null) {
+    pageContent = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    pageContent = <NoProjectSelected onClickAddProjectButton={handleAddProjectButton}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NoProjectSelected />
+      <Sidebar onClickAddProjectButton={handleAddProjectButton}/>
+      {pageContent}
     </main>
   );
 }
